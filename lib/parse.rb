00948628193990081dbf6639d96dee337f9fe9b2
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-02-17 14:15:19 -0300

    Generate bdd from test_case_hash

--- lib/parse.rb
+++ lib/parse.rb
@@ -42,3 +42,31 @@ def test_cases_hash(example_name)
 
   test_cases
 end
+
+def event_to_s(event)
+  "#{event[:sender]} send #{event[:action]} to #{event[:receiver]}"
+end
+
+def test_case_tag(event, test_case)
+  if event == test_case.last
+    "Then"
+  elsif event == test_case.first
+    "When"
+  else
+    "And"
+  end
+end
+
+def bdd_from_test_cases(test_cases_hash, feature_name = "Test")
+  File.open "output/#{feature_name}.feature", "w" do |file|
+    file.write("Feature: #{feature_name}\n")
+    test_cases_hash.each_with_index do |test_case, index|
+      file.write("\tScenario: #{index}\n")
+      test_case.each do |event|
+        file.write("\t\t#{test_case_tag(event, test_case)} #{event_to_s(event)}\n")
+      end
+    end
+  end
+end
+
+bdd_from_test_cases(test_cases_hash("BankTellerExample"))

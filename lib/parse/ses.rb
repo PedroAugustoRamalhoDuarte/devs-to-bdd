456f255e943873feb729f575591b5bba3de66160
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2022-05-04 10:21:12 -0300

    Adds next line ses checker for works with bsn

--- lib/parse/ses.rb
+++ lib/parse/ses.rb
@@ -22,16 +22,24 @@ module Parse
 
       event_list = []
       test_cases = []
-      file_data.each do |line|
+      file_data.each_with_index do |line, index|
         # Extract components
         if line.include? 'is made of'
-          # passs
+          # pass
         elsif line.include? 'From the' # Extract events
           event = extract_event(line)
           event_list.append(event)
 
           # If the first sender is now the receiver
           if event.receiver == event_list[0].sender
+            # TODO: Check this logic (BSNExample vs JazzBand)
+            # Check if next line sender is receiver
+            next_line = file_data[index + 1]
+            next_line_event = extract_event(next_line)
+            if (next_line_event.sender == event.receiver) && (file_data.length == index + 2)
+              event_list.append(next_line_event)
+            end
+
             # Closes one test case
             test_cases.append(event_list.dup)
             event_list.clear
